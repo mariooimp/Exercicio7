71312a4163401db48b7a5a68e6e4e533e6b43761
mario <mario@Mario>
2025-05-26 09:45:25 -0300

    Merge origin/main into main

---
+++ src/exercicio7/Exercicio7.java
@@ -0,0 +1,10 @@
+package exercicio7;
+
+import javax.swing.JOptionPane;
+public class Exercicio7 {
+
+    public static void main(String[] args) {
+		String nome;
+		nome = JOptionPane.showInputDialog("Insira um nome: ");
+		JOptionPane.showMessageDialog(null, "O nome é: "+nome); }
+		}
